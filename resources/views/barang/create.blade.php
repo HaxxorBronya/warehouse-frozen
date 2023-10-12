ea283e70b99871c32da954fdc3508ab6bbb17a06
vcto <victoa.rpl@gmail.com>
2023-10-12 10:46:33 +0700

    fixed bug

--- resources/views/barang/create.blade.php
+++ resources/views/barang/create.blade.php
@@ -2,80 +2,86 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Form Edit Barang</title>
+    <title>Tambah Barang</title>
  
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
 </head>
 <body>
  
 <div class="container mt-2">
- 
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Edit Barang</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('barangs.index') }}" enctype="multipart/form-data"> Back</a>
-            </div>
+   
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left mb-2">
+            <h2>Tambah Barang</h2>
         </div>
     </div>
+</div>
     
   @if(session('status'))
     <div class="alert alert-success mb-1 mt-1">
         {{ session('status') }}
     </div>
   @endif
-   
-    <form action="{{ route('barangs.update',$barang->id) }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
     
-         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Nama Barang</strong>
-                    <input type="text" name="nama_barang" value="{{ $barang->nama_barang }}" class="form-control" placeholder="Nama Barang">
-                    @error('nama_barang')
-                     <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                    @enderror
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Kode Barang</strong>
-                     <input type="text" name="kode_barang" class="form-control" placeholder="Kode Barang" value="{{ $barang->kode_barang }}">
-                    @error('kode_barang')
-                      <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                   @enderror
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Harga Barang</strong>
-                    <input type="number" name="harga_barang" value="{{ $barang->harga_barang }}" class="form-control" placeholder="Harga Barang">
-                    @error('harga_barang')
-                    <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                    @enderror
-                </div>
-            </div>
-             <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Jumlah Barang</strong>
-                    <input type="number" name="jumlah_barang" value="{{ $barang->jumlah_barang }}" class="form-control" placeholder="Jumlah Barang">
-                    @error('jumlah_barang')
-                     <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                    @enderror
-                </div>
-            </div>
-              <button type="submit" class="btn btn-primary ml-3">Submit</button>
+<div class="card">
+    <div class="card-body">
+        <form action="{{ route('barangs.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
            
-        </div>
-    
-    </form>
+             <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Nama Barang</strong>
+                        <input type="text" name="nama_barang" class="form-control" placeholder="Nama Barang">
+                       @error('nama_barang')
+                          <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                       @enderror
+                    </div>
+                </div>
+         
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Kode Barang</strong>
+                         <input type="text" name="kode_barang" class="form-control" placeholder="Kode Barang">
+                        @error('kode_barang')
+                          <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                       @enderror
+                    </div>
+                </div>
+         
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Harga Barang</strong>
+                         <input type="number" name="harga_barang" class="form-control" placeholder="Harga Barang">
+                        @error('harga_barang')
+                          <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                       @enderror
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Jumlah Barang</strong>
+                         <input type="number" name="jumlah_barang" class="form-control" placeholder="Jumlah Barang">
+                        @error('harga_barang')
+                          <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                       @enderror
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <a class="btn btn-light" href="{{ route('barangs.index') }}"> Back</a>
+                    <button type="submit" class="btn btn-primary ml-3">Submit</button>
+                </div>
+            </div>
+            
+        </form>
+        
+    </div>
 </div>
- @error('name')
+@error('name')
 <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
 @enderror
+ 
 </body>
 </html>
--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -2,56 +2,67 @@
 
 @section('content')
  
-<div class="container mt-2">
- 
-<div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Inventory Page</h2>
+<div class="mt-2">
+    <div class="card shadow-sm">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-lg-12 margin-tb">
+                    <div class="pull-left">
+                        <h2>Inventory Page</h2>
+                    </div>
+                    <div class="pull-right mb-2">
+                        <a class="btn btn-dark" href="{{ route('barangs.create') }}">Tambah Barang</a>
+                    </div>
+                </div>
             </div>
-            <div class="pull-right mb-2">
-                <a class="btn btn-success" href="{{ route('barangs.create') }}">Tambah Barang</a>
-            </div>
+            
+            @if ($message = Session::get('success'))
+                <div class="alert alert-success">
+                    <p class="m-0 p-0">{{ $message }}</p>
+                </div>
+            @endif
+            
+            <table class="table table-bordered table-striped">
+                <thead class="table-dark">
+                    <th>No</th>
+                    <th>Nama Barang</th>
+                    <th>Kode Barang</th>
+                    <th>Harga Barang</th>
+                    <th>Jumlah Barang</th>
+                    <th width="280px">Action</th>
+                </thead>
+                <tbody>
+                    @php
+                    $no = 1;
+                    @endphp
+                    @foreach ($barangs as $barang)
+                    <tr>
+                        <td>{{ $no++ }}</td>
+                        <td>{{ $barang->nama_barang }}</td>
+                        <td>{{ $barang->kode_barang }}</td>
+                        <td>{{ $barang->harga_barang }}</td>
+                        <td>{{ $barang->jumlah_barang }}</td>
+                        <td>
+                            <form action="{{ route('barangs.destroy',$barang->id) }}" method="Post">
+                 
+                                <a class="btn btn-sm btn-light" href="{{ route('barangs.edit',$barang->id) }}">Edit</a>
+                
+                                @csrf
+                                @method('DELETE')
+                   
+                                <button type="submit" class="btn btn-sm btn-light">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                    
+                </tbody>
+            </table>
+           
+            {!! $barangs->links() !!}
+            
         </div>
     </div>
-    
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
-    
-    <table class="table table-bordered">
-        <tr>
-            <th>No</th>
-            <th>Nama Barang</th>
-            <th>Kode Barang</th>
-            <th>Harga Barang</th>
-            <th>Jumlah Barang</th>
-            <th width="280px">Action</th>
-        </tr>
-        @foreach ($barangs as $barang)
-        <tr>
-            <td>{{ $barang->id }}</td>
-            <td>{{ $barang->nama_barang }}</td>
-            <td>{{ $barang->kode_barang }}</td>
-            <td>{{ $barang->harga_barang }}</td>
-            <td>{{ $barang->jumlah_barang }}</td>
-            <td>
-                <form action="{{ route('barangs.destroy',$barang->id) }}" method="Post">
-     
-                    <a class="btn btn-primary" href="{{ route('barangs.edit',$barang->id) }}">Edit</a>
-    
-                    @csrf
-                    @method('DELETE')
-       
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-   
-    {!! $barangs->links() !!}
+ 
  
 @endsection
